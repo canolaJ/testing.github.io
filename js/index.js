13f39490c9d86202988b8153ef9368665db9b072
Jorge Hernán Cañola Salazar <95511124+canolaJ@users.noreply.github.com>
2023-11-19 15:52:12 -0500

    update index,js
    se actualizae el riseze de las imagenes cuando se cortan

--- js/index.js
+++ js/index.js
@@ -3,9 +3,14 @@ let cropper = null;
 $('#input-file').on('change', () => {
     let image = document.getElementById('img-cropper')
     let input = document.getElementById('input-file')
+    //convierto imagen base 64 a un objecto IMG para poder editarlo --> var imagen1 = new Image(600, 400);
+    //let img = new Image();
+    //let img64 = ""
+    //image.src = 'data:image/png;base64,'+img64; 
 
     let archivos = input.files
     let extensiones = input.value.substring(input.value.lastIndexOf('.'), input.value.lenght)
+   
     
 
     if(!archivos || !archivos.length){        
@@ -19,9 +24,9 @@ $('#input-file').on('change', () => {
     } else {
         let imagenUrl = URL.createObjectURL(archivos[0])
         image.src = imagenUrl
-
+        // console.log(image)
         cropper = new Cropper(image, {
-            aspectRatio: 1/3, // es la proporción en la que queremos que recorte en este caso 1:1
+            aspectRatio: 1.28/2, // es la proporción en la que queremos que recorte en este caso 1:1
             preview: '.img-sample', // contenedor donde se va a ir viendo en tiempo real la imagen cortada
             zoomable: true, //Para que no haga zoom 
             viewMode: 1, //Para que no estire la imagen al contenedor
@@ -30,15 +35,22 @@ $('#input-file').on('change', () => {
             cropBoxResizable:false,
             minContainerWidth:0,
             minContainerHeight:0,
-            minCanvasWidth:650,
+            minCanvasWidth:300,
             minCanvasHeight:1050,
-            minCropBoxWidth:650,
-            minCropBoxHeight:1030,
+            minCropBoxWidth:672,
+            minCropBoxHeight:1050,
+
             ready(){ // metodo cuando cropper ya este activo, le ponemos el alto y el ancho del contenedor de cropper al 100%
                 document.querySelector('.cropper-container').style.width = '100%'
                 document.querySelector('.cropper-container').style.height = '100%'
+                cropper.setCropBoxData({
+                    width: 100,
+                    height: 1050,
+                });
+                  
             }
         })
+        console.log(cropper);
 
         $('.modal').addClass('active')
         $('.modal-content').addClass('active')
@@ -66,14 +78,22 @@ $('#close').on('click', () => {
 
 $('#cut').on('click', () => {
     let crop_image = document.getElementById('crop-image')
-    let canva = cropper.getCroppedCanvas()
+    let canva = cropper.getCroppedCanvas({
+        minWidth: 672,
+        minHeight: 1050,
+        maxWidth: 672,
+        maxHeight: 1050,
+    })
     let image = document.getElementById('img-cropper')
     let input = document.getElementById('input-file')
-
+    let imagenEn64 = canva.toDataURL("image/jpeg");
+    let imagenEn64Split = imagenEn64.slice(23)
+    console.log(imagenEn64);
+    //console.log(imagenEn64Split);
     canva.toBlob(function(blob){
         let url_cut = URL.createObjectURL(blob)
         crop_image.src = url_cut;
-        console.log(url_cut);
+    
     })
 
     image.src = "";
